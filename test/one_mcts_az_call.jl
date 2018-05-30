d4fee18687286b02c8763384e5b1b78365eb8c78
carljohanhoel <carl-johan.hoel@chalmers.se>
2018-05-31 01:04:15 +0200

    added replay memory handling

--- test/one_mcts_az_call.jl
+++ test/one_mcts_az_call.jl
@@ -27,10 +27,11 @@ state = GridWorldState(1,1)
 
 n_s = length(MCTS.convert_state(state))
 n_a = n_actions(mdp)
-
+replay_memory_max_size = 55
+training_start = 40
 estimator_path = "/home/cj/2018/Stanford/Code/Multilane.jl/src/nn_estimator"
 log_path = "/home/cj/2018/Stanford/Code/Multilane.jl/Logs/"*Dates.format(Dates.now(), "yymmdd_HHMMSS")
-estimator = NNEstimator(rng, estimator_path, log_path, n_s, n_a)
+estimator = NNEstimator(rng, estimator_path, log_path, n_s, n_a, replay_memory_max_size, training_start)
 
 load_network(estimator,"/home/cj/2018/Stanford/Code/Multilane.jl/Logs/180530_022108/50001")
 # load_network(estimator,"/home/cj/2018/Stanford/Code/Multilane.jl/Logs/180530_200610/10001")
--- test/one_training_run.jl
+++ test/one_training_run.jl
@@ -13,8 +13,25 @@ n_iter = 1000
 depth = 15
 c_puct = 10.0
 
-rng=MersenneTwister(53)
+# replay_memory_max_size = 55
+# training_start = 40
+#
+# training_steps = 100
+# save_freq = 20
+# eval_freq = 20
+# eval_eps = 3
+training_steps = 100000
+save_freq = 5000
+eval_freq = 5000
+eval_eps = 100
+replay_memory_max_size = 100000
+training_start = 5000
 
+sim_max_steps = 20
+
+
+
+rng=MersenneTwister(53)
 
 mdp = GridWorld(5,5,
                 penalty=0.,
@@ -28,7 +45,7 @@ n_s = length(MCTS.convert_state(s_initial))
 n_a = n_actions(mdp)
 estimator_path = "/home/cj/2018/Stanford/Code/Multilane.jl/src/nn_estimator"
 log_path = "/home/cj/2018/Stanford/Code/Multilane.jl/Logs/"*Dates.format(Dates.now(), "yymmdd_HHMMSS")
-estimator = NNEstimator(rng, estimator_path, log_path, n_s, n_a)
+estimator = NNEstimator(rng, estimator_path, log_path, n_s, n_a, replay_memory_max_size, training_start)
 
 solver = AZSolver(n_iterations=n_iter, depth=depth, exploration_constant=c_puct,
                k_state=3.,
@@ -43,9 +60,9 @@ solver = AZSolver(n_iterations=n_iter, depth=depth, exploration_constant=c_puct,
 policy = solve(solver, mdp)
 
 
-sim = HistoryRecorder(rng=rng, max_steps=20, show_progress=false)
+sim = HistoryRecorder(rng=rng, max_steps=sim_max_steps, show_progress=false)
 
 ##
-trainer = Trainer(rng=rng, training_steps=100, save_freq=20, eval_freq=20, eval_eps=3, show_progress=true, log_dir=log_path)
+trainer = Trainer(rng=rng, training_steps=training_steps, save_freq=save_freq, eval_freq=eval_freq, eval_eps=eval_eps, show_progress=true, log_dir=log_path)
 # trainer = Trainer(rng=rng, training_steps=100000, save_freq=5000, eval_freq=5000, eval_eps=100, show_progress=true, log_dir=log_path)
 train(trainer, sim, mdp, policy)
